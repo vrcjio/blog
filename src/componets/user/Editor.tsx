88322051c5f23ac8fc68be04fba605b05da29db6
vrcjio <132543591+vrcjio@users.noreply.github.com>
2023-09-27 11:05:55 +0530

    fixed deploy bug

--- src/componets/user/Editor.tsx
+++ src/componets/user/Editor.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { addEditorData } from "@/lib/Redux/EditorData";
+import { addEditorData } from "@/lib/Redux/editorData";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import React from "react";
