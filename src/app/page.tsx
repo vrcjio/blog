efaf90a22f2f0f77764c20b481d8583b192c6830
vrcjio <132543591+vrcjio@users.noreply.github.com>
2023-09-27 13:16:39 +0530

    function export problem solver2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Carousel from "@/componets/carousel"
 import TopBar from "@/componets/topbar"
 import UserArticals from "@/componets/userArticals"
 import LoadingInit from "@/util/systemInit"
-export default function HomePage{
+export default function HomePage(){
   return (
     <>
     <LoadingInit />
