c02dde83a794c5aa9e26193df1b25090a939f9c3
vrcjio <132543591+vrcjio@users.noreply.github.com>
2023-09-29 11:30:00 +0530

    new page testing

---
+++ src/app/contactUs/page.tsx
@@ -0,0 +1,15 @@
+import Footer from '@/componets/footer'
+import TopBar from '@/componets/topbar'
+import React from 'react'
+
+function ContactUs() {
+  return (
+    <>
+      <TopBar />
+      <div>ContactUs</div>
+      <Footer />
+    </>
+  )
+}
+
+export default ContactUs
